32f6ce9257ff3428bb25c8558e56e0c2ad90630e
toshisugar <39898922+toshisugar@users.noreply.github.com>
2019-02-21 19:11:43 +0900

    プロジェクト ファイルを追加します。

---
+++ CordingTraning_from20190221/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CordingTraning_from20190221.MvcApplication" Language="C#" %>
---
+++ CordingTraning_from20190221/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CordingTraning_from20190221.Startup))]
+namespace CordingTraning_from20190221
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
